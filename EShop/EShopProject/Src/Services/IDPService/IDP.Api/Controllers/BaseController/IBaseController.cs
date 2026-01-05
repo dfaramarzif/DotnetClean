d8ab07394fd8c793858b96bb16c7811ead95f809
d.faramarzi <dfaramarzifilabadi@gmail.com>
2026-01-05 16:20:21 +0330

    #Add base project

---
+++ EShop/EShopProject/Src/Services/IDPService/IDP.Api/Controllers/BaseController/IBaseController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IDP.Api.Controllers.BaseController
+{
+    public class IBaseController : Controller
+    {
+       
+    }
+}
---
+++ EShop/EShopProject/Src/Services/IDPService/IDP.Api/Controllers/V1/UserController.cs
@@ -0,0 +1,13 @@
+﻿using IDP.Api.Controllers.BaseController;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IDP.Api.Controllers.V1
+{
+    public class UserController : IBaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
